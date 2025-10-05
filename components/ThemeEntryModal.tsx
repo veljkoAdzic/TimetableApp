7ea0bfe8afbf97fe3888d91144cfd46aaa10f294
Veljko.A <veljkoadzic5@gmail.com>
2025-10-05 22:47:18 +0200

    - Added reanimated-color-picker package - Changed styling and structure of ColourButton - Added tabs to change colour of background or text - Added colour picker next to preview - updated some styling

--- components/ThemeEntryModal.tsx
+++ components/ThemeEntryModal.tsx
@@ -3,17 +3,19 @@ import {ThemeEditingContext} from '@/constants/Contexts'
 import { useContext, useState } from 'react'
 import { EventColorsType, EventColors } from '@/constants/EventColors'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
-
+import ColorPicker, {Panel1, Swatches, Preview, OpacitySlider, HueSlider, InputWidget, SaturationSlider, LuminanceSlider, BrightnessSlider } from 'reanimated-color-picker'
 function ColourButton(props:{theme: EventColorsType, onClick:() => void}){
 
     return (
         <Pressable onPress={() => {           
                 props.onClick()
             } } 
-        style={{padding: 2, borderRadius: '50%', borderWidth: 1, borderColor: '#DDD'}} 
-        >
-            <View style={[styles.themeButtonMain, {backgroundColor: props.theme.background}]}>
-                <View style={[styles.themeButtonSecondary, {backgroundColor: props.theme.text}]} />
+            style={{padding: 4}}
+            >
+            <View style={{padding: 2, borderRadius: '50%', borderWidth: 1, borderColor: '#DDD', justifyContent: 'center', alignItems: 'center', elevation: 2, backgroundColor: '#F5F5F5'}} >
+                <View style={[styles.themeButtonMain, {backgroundColor: props.theme.background}]}>
+                    <View style={[styles.themeButtonSecondary, {backgroundColor: props.theme.text}]} />
+                </View>
             </View>
         </Pressable>
     )
@@ -21,6 +23,7 @@ function ColourButton(props:{theme: EventColorsType, onClick:() => void}){
 
 export default function ThemeEntryModal(){
     const {entry, setEntry, saveChanges} = useContext(ThemeEditingContext)
+    const [tabFocused, setTabFocused] = useState<'background' | 'text'>('background')
 
     if (entry == null){
         return <></>
@@ -40,6 +43,21 @@ export default function ThemeEntryModal(){
                 />
 
                 <View style={styles.modalView}>
+
+                
+
+                    <View style={styles.tabBar}>
+                        <Pressable style={[styles.tab, (tabFocused != 'background' ? {backgroundColor: '#DDD', borderBottomRightRadius: 5} : {})]}
+                        onPress={() => {setTabFocused('background')}}>
+                            <Text style={{}}>Background</Text>
+                        </Pressable>
+
+                        <Pressable style={[styles.tab, (tabFocused != 'text' ? {backgroundColor: '#DDD', borderBottomLeftRadius: 5} : {})]}
+                        onPress={() => {setTabFocused('text')}}>
+                            <Text>Text</Text>
+                        </Pressable>
+                    </View>
+
                     <Text style={styles.title}>
                         {entry[0]}
                     </Text>
@@ -47,7 +65,7 @@ export default function ThemeEntryModal(){
                     <View style={styles.previewPickerContainer}>
 
                         <View style={styles.colourPickerGroup}>
-                            <Text style={{fontSize: 15}}>Preview</Text>
+                            {/* <Text style={{fontSize: 15}}>Preview</Text> */}
                         <View style={[styles.previewBlock, {backgroundColor: entry[1].background, borderColor: entry[1].border}]}>
                             <Text style={{color: entry[1].text}}>Title</Text>
                             <Text style={{color: entry[1].text, fontSize:10, opacity: 0.5}}>Sub Text 1</Text>
@@ -55,18 +73,26 @@ export default function ThemeEntryModal(){
                         </View>
                         </View>
 
-                        <View style={styles.colourPickerGroup}>
-                            <Text style={{fontSize: 15}}>Current</Text>
-
-                            <View style={styles.pickerContainer}>
-                                <Pressable onPress={() => { console.log("L") }} style={{flexGrow: 1, borderTopLeftRadius: 7, borderTopRightRadius: 7, backgroundColor: entry[1].background}} />
-
-                                <Pressable onPress={() => { console.log("R")}} style={{flexGrow: 1, borderBottomLeftRadius: 7, borderBottomRightRadius: 7, backgroundColor: entry[1].text}} />
+                        <View style={[styles.colourPickerGroup]}>
+                        <ColorPicker 
+                        value={entry[1][tabFocused]} 
+                        boundedThumb={true} 
+                        onChangeJS={({hex}) => { 
+                            let newColor = {...entry[1]}
+                            newColor[tabFocused] = hex
+                            setEntry([entry[0], newColor])
+                        } } 
+                        >
+                            <View style={{display: 'flex', flexDirection: 'column', gap: 10, width: 170, backgroundColor: '#FAFAFA', padding: 10, borderRadius: 5, borderWidth: 1, borderColor: '#DDD'}}>
+                                <Panel1 style={{aspectRatio: 1/1, width: '100%', height: 'auto'}} />
+                                
+                                <HueSlider style={{}} />
                             </View>
+                        </ColorPicker>
                         </View>
                     </View>
 
-                    <FlatList fadingEdgeLength={15} style={styles.coloursContainer} contentContainerStyle={{gap: 15, padding: 10, paddingBottom: 20}} snapToInterval={50}
+                    <FlatList fadingEdgeLength={15} style={styles.coloursContainer} contentContainerStyle={{gap: 15, padding: 10, paddingBottom: 20}}
                         data={Array(Math.ceil(EventColors.length/3)).map((_, ind) =>  {return [ind*3, ind*3+1, ind*3+2] })}
                         initialNumToRender={9}
                         renderItem={ ({item, index}) => 
@@ -95,7 +121,9 @@ export default function ThemeEntryModal(){
                     </View>
 
                 </View>
+
             </View>
+
         </Modal>
     )
 }
@@ -111,30 +139,51 @@ const styles = StyleSheet.create({
         margin: 20,
         backgroundColor: 'white',
         borderRadius: 20,
-        padding: 35,
-        paddingBottom: 5,
+        // padding: 35,
+        // paddingVertical: 5,
         alignItems: 'center',
         elevation: 5,
         width: "80%",
         gap: 25,
+        overflow: 'hidden',
     },
-    title: {
+
+    tabBar: {
         width: '100%', 
+        display: 'flex', 
+        flexDirection: 'row', 
+        overflow: 'hidden', 
+        height: 45,
+        paddingBottom: 3,
+        borderBlockColor: '#EEE',
+        borderBottomWidth: 1,
+    },
+
+    tab: {
+        width: '50%', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        backgroundColor: 'white'
+    },
+
+    title: {
+        width: '80%', 
         fontSize: 18, 
-        borderBottomColor: '#111', 
+        borderBottomColor: '#1115', 
         borderBottomWidth: 1, 
-        paddingLeft: 5, 
+        paddingLeft: 5,
     },
 
     previewPickerContainer: {
         display: 'flex', 
         flexDirection: 'row', 
-        gap: 15, 
-        alignItems: 'flex-start', 
-        justifyContent: 'space-evenly',
+        gap: 15,  
+        justifyContent: 'space-between',
+        padding: 5,
         borderBottomWidth: 1,
         borderColor: "#11111155",
-        width: '100%',
+        width: '80%',
+        minHeight: '30%',
     },
 
     previewBlock: {
@@ -142,16 +191,17 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 6,
         minWidth: 80,
-        minHeight: 80,
+        minHeight: 90,
         
     },
 
     colourPickerGroup: {
         display: 'flex', 
         gap: 15, 
-        alignItems: 
-        'center',
+        alignItems: 'center',
+        justifyContent: 'center',
         paddingBottom: 15,
+        height: 'auto',
     },
 
     pickerContainer: {
@@ -170,6 +220,7 @@ const styles = StyleSheet.create({
     coloursContainer: {
         display: 'flex', 
         height: "12%",
+        width: '80%',
     },
 
     themeButtonMain: {
@@ -177,7 +228,7 @@ const styles = StyleSheet.create({
         aspectRatio: 1/1,
         borderRadius: '50%',
         overflow: 'hidden',
-        elevation: 3,
+        // elevation: 3,
     },
     themeButtonSecondary: {
         width: '150%',
@@ -185,7 +236,7 @@ const styles = StyleSheet.create({
         margin: '35%',
         transform: [{rotateZ: '45deg'}],
 
-        borderColor: '#d4d4d4d3',
+        borderColor: '#F5F5F5',
         borderWidth: 2.5,
     },
 
@@ -194,9 +245,9 @@ const styles = StyleSheet.create({
         flexDirection:'row', 
         alignItems: 'center', 
         justifyContent: 'space-around',
-        width: '100%',
+        width: '80%',
         borderTopWidth: 1,
-        borderColor: '#151515'
+        borderColor: '#1115',
     },
     editBarButton: {
         padding: 10,
