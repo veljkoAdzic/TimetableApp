d97faef21557b58e745a96aedaa9cd38fbcdf265
veljkoAdzic <veljkoadzic5@gmail.com>
2026-02-14 16:18:55 +0100

    - Changed ShareScreen.tsx layout - Upgraded link share protocol

--- app/ShareScreen.tsx
+++ app/ShareScreen.tsx
@@ -1,153 +1,181 @@
-import {View, Text, StyleSheet, Pressable, ActivityIndicator} from 'react-native'
-import {useState, useEffect, useCallback} from 'react'
+import {View, Text, StyleSheet, Pressable, ActivityIndicator, Share, PermissionsAndroid, Platform} from 'react-native'
+import React, {useState, useEffect, useCallback, useRef, forwardRef} from 'react'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { getCompressedData, encodeB94, SHARE_LINK_BASE } from '@/utils/encoding'
-import QRCode from 'react-native-qrcode-svg'
 import { TextInput } from 'react-native-gesture-handler'
 import * as Clipboard from 'expo-clipboard'
 import { useFocusEffect } from 'expo-router'
 
 export default function ShareScreen() {
-    const OPTIONS = [
-    {
-        iconName: 'image-move',
-        text: 'Share as image'
-    },
-    {
-        iconName: 'qrcode',
-        text: 'Share as QR code or link'
-    },
-    {
-        iconName: 'file-download',
-        text: 'Export as file'
-    },
-    ]
+    const [shareLink, setShareLink] = useState<string|null>(null)
+    const [errMsg, setErrMsg] = useState<string|null>(null)
 
     useFocusEffect(
         useCallback(()=>{ // Memoising the function
-            setQRdata(null)
             setShareLink(null)
             setErrMsg(null)
-            setSelected(0)
-            
-            return () =>{} // must return function
-    
-        }, [])
-        );
 
-    const [selected, setSelected] = useState(0)
-
-    const [errMsg, setErrMsg] = useState<string|null>(null)
-
-    const [QRdata, setQRdata] = useState<string|null>(null)
-    const [shareLink, setShareLink] = useState<string|null>(null)
-
-
-    useEffect(() => {
-        if(selected == 1 && QRdata == null && shareLink == null) {
             getCompressedData()
             .then(arrBytes => {
-                const parts: string[] = new Array(arrBytes.length)
-                for( let i = 0; i < arrBytes.length; i++){
-                    parts[i] = String.fromCharCode(arrBytes[i])
-                }
                 setShareLink(SHARE_LINK_BASE + encodeB94(arrBytes))
-                setQRdata(parts.join(""))
                 setErrMsg(null)
             })
             .catch((e) => {
                 setErrMsg(e instanceof Error ? e.message : String(e))
             })
-            
-        }
-    }, [selected])
 
+            return () =>{} // must return function
+        }, [])
+        );
+    
+    // const btnColours = ["#8D80FF", "#6AB879", "#EC6D7A"]
+    const btnColours = ["#6F66E2", "#489659", "#CE5362"]
+
+    const handleLinkShare = async () => {
+        try {
+            if (shareLink == null) 
+                throw "Link not ready!"
+
+            const result = await Share.share({
+                message: shareLink
+            });
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                // shared with activity type of result.activityType
+                } else {
+                // shared
+                }
+            } else if (result.action === Share.dismissedAction) {
+                // dismissed
+            }
+        } catch (error: any) {
+        //   Alert.alert(error.message);
+            setErrMsg(error.message)
+        };
+    }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>{OPTIONS[selected].text}</Text>
+            <View style={styles.section}>  
+                <Text style={styles.label}>Share as link:</Text> 
 
-            { selected == 0 && errMsg == null && /* EXPORT AS IMAGE */
-                <View style={[{backgroundColor: 'lime'}, styles.mainContainer]}>
-                    <Text>IMG</Text>
-                </View>
-            }
+                <View style={styles.btnGroup}>
+                    <Pressable 
+                    onPress={() => { if(shareLink) Clipboard.setStringAsync(shareLink) }}
+                    style={[styles.btn, {backgroundColor: btnColours[0]}]}
+                    > 
+                    { ({pressed}) => (
+                        <View style={[
+                        styles.btnContent, 
+                        pressed ? { backgroundColor: '#FFF3' }: {}
+                    ]}>
+                        <Text style={styles.btnText}>Copy</Text>
 
-            {errMsg == null && /* QR CODE */
-                <View style={[styles.mainContainer, (selected==1) ? {} : {display: 'none'}]}>                    
-                    { (QRdata == null && shareLink == null) ?
-                    <View style={{flex: 1, justifyContent: 'center'}}> 
-                        <ActivityIndicator color="#6139cf" size={50} /> 
-                    </View> :
-                    <>
-                    <View style={{borderRadius: 15, overflow: 'hidden', elevation: 5}}>
-                    <QRCode value={QRdata!} size={310} ecl='L' quietZone={20} />
-                    </View>
-
-                    <View style={styles.shareLinkContainer}>
-                        <TextInput 
-                        style={{backgroundColor: '#FFF', color: '#555', borderRadius: 10, padding: 10, flexShrink: 1, elevation: 3, borderWidth: 1, borderColor: '#1113' }}
-                        value={shareLink || ''}
-                        scrollEnabled
-                        selection={{start: 0}}
-                        editable={false}
-                                            
+                        <MaterialCommunityIcons 
+                        name="clipboard-outline" 
+                        size={styles.btnText.fontSize + 7} 
+                        color={styles.btnText.color}
                         />
-                        <Pressable 
-                        onPress={() => { if(shareLink) Clipboard.setStringAsync(shareLink) }}
-                        style={{overflow: 'hidden', borderRadius: '30%', backgroundColor: '#555ee4ff', elevation: 3}}
-                        > 
-                        { ({pressed}) => (
-                            <MaterialCommunityIcons 
-                            name="clipboard-outline" 
-                            size={32} 
-                            color='#EAEAEA' 
-                            style={[{padding: 5}, pressed ? { backgroundColor: '#FFF3' }: {}]} 
-                            />
-                        ) } 
-                        </Pressable>
                     </View>
-                    </>
-                    }
-                </View>
-            }
-
-            { selected == 2 && errMsg == null && /* EXPORT AS FILE */
-                <View style={[{backgroundColor: 'yellow'}, styles.mainContainer]}>
-                    <Text>FILE</Text>
-                </View>
-            }
-
-
-            { errMsg != null &&
-                <View style={[{backgroundColor: 'pink'}, styles.mainContainer]}>
-                    <Text>{errMsg}</Text>
-                </View>
-            }
-
-            <View style={styles.optionsContainer}>
-
-                {OPTIONS.map((val, i) => (
-                    <Pressable key={i} style={{padding: 5}} 
-                    onPress={() => {setSelected(i)}} >
-                        { ({pressed}) => (
-                        <View style={[styles.optionsItem, (i==selected && styles.optionsItemSelected), (pressed && {})]}>
-                            <MaterialCommunityIcons 
-                            name={val.iconName} 
-                            size={30} 
-                            color={(i == selected) ? "#FFF" : "#111"} 
-                            style={[
-                                {padding: 15}, 
-                                (pressed) && {backgroundColor: ((i==selected) ? '#FFF3' : '#4442')}
-                            ]} />
-                        </View>
-                        )
-                        }
+                    ) }
                     </Pressable>
-                )) }
-            </View>
-            
 
+                    <Pressable 
+                    onPress={handleLinkShare}
+                    style={[styles.btn, {backgroundColor: btnColours[0]}]}
+                    > 
+                    { ({pressed}) => (
+                        <View style={[
+                        styles.btnContent, 
+                        pressed ? { backgroundColor: '#FFF3' }: {}
+                    ]}>
+                        <Text style={styles.btnText}>Share</Text>
+
+                        <MaterialCommunityIcons 
+                        name="link-variant" 
+                        size={styles.btnText.fontSize + 7} 
+                        color={styles.btnText.color}
+                        />
+                    </View>
+                    ) }
+                    </Pressable>
+                </View>
+            </View>
+
+            <View style={styles.section}>
+                <Text style={styles.label}>Share as images:</Text>
+
+                <View style={styles.btnGroup}>
+                    <Pressable 
+                    onPress={() => { /* TODO */}}
+                    style={[styles.btn, {backgroundColor: btnColours[1]}]}
+                    > 
+                    { ({pressed}) => (
+                        <View style={[
+                        styles.btnContent, 
+                        pressed ? { backgroundColor: '#FFF3' }: {}
+                    ]}>
+                        <Text style={styles.btnText}>Save</Text>
+
+                        <MaterialCommunityIcons 
+                        name="download" 
+                        size={styles.btnText.fontSize + 7} 
+                        color={styles.btnText.color}
+                        />
+                    </View>
+                    ) }
+                    </Pressable>
+
+                    <Pressable 
+                    onPress={() => { /* TODO */}}
+                    style={[styles.btn, {backgroundColor: btnColours[1]}]}
+                    > 
+                    { ({pressed}) => (
+                        <View style={[
+                        styles.btnContent, 
+                        pressed ? { backgroundColor: '#FFF3' }: {}
+                    ]}>
+                        <Text style={styles.btnText}>Share</Text>
+
+                        <MaterialCommunityIcons 
+                        name="image-move" 
+                        size={styles.btnText.fontSize + 7} 
+                        color={styles.btnText.color}
+                        />
+                    </View>
+                    ) }
+                    </Pressable>
+                </View>
+            </View>
+
+            <View style={styles.section}>
+                <Text style={styles.label}>Share as file:</Text> 
+                
+                <View style={styles.btnGroup}>
+
+                <Pressable 
+                onPress={() => { /* TODO */}}
+                style={[styles.btn, {backgroundColor: btnColours[2]}]}
+                > 
+                { ({pressed}) => (
+                    <View style={[
+                        styles.btnContent, 
+                        pressed ? { backgroundColor: '#FFF3' }: {}
+                    ]}>
+                        <Text style={styles.btnText}>Export</Text>
+
+                        <MaterialCommunityIcons 
+                        name="file-move" 
+                        size={styles.btnText.fontSize + 7} 
+                        color={styles.btnText.color}
+                        />
+                    </View>
+                ) } 
+                </Pressable>
+
+                </View>
+
+            </View>
         </View>
     )
 }
@@ -158,10 +186,11 @@ const styles = StyleSheet.create({
         flex: 1,
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'flex-start',
+        justifyContent: 'center',
         alignItems: 'stretch',
         padding: 40,
-        gap: 30,
+        paddingTop: -100,
+        gap: 60,
     },
 
     title: {
@@ -192,11 +221,14 @@ const styles = StyleSheet.create({
         elevation: 7
     },
 
-    mainContainer: {
+    section: {
         width: '100%', 
-        flexGrow: 1, 
         alignItems: 'center', 
-        gap: 40
+        gap: 10,
+
+        // paddingBottom: 20,
+        // borderBottomWidth: 1,
+        // borderColor: '#dadada'
     },
 
     shareLinkContainer: {
@@ -205,5 +237,53 @@ const styles = StyleSheet.create({
         alignItems: 'center', 
         width: "100%", 
         gap: 10,
+    },
+    label: {
+        width: '100%', 
+        fontSize: 30,
+        color: '#6c6288',
+        paddingLeft: 10
+    },
+
+    loaderWrapper: {
+        justifyContent: 'center', 
+        backgroundColor: "#ededed",
+        width: '100%', 
+        padding: 5, 
+        borderRadius: 10
+    },
+
+    btnGroup: {
+        display: 'flex', 
+        flexDirection: 'row', 
+        justifyContent: 'space-around', 
+        width: '100%', 
+        gap: 20
+    },
+
+    btn: {
+        overflow: 'hidden', 
+        borderRadius: 10, 
+        backgroundColor: '#4872e6', 
+        elevation: 3, 
+        flexGrow: 1,
+        maxWidth: '60%'
+    },
+
+    btnContent: {
+        flexDirection: 'row', 
+        alignItems: 'center',
+        justifyContent: 'space-evenly', 
+        padding: 7, 
+        gap: 10,
+    },
+
+    btnPress: {
+        backgroundColor: '#FFF3'
+    },
+
+    btnText: {
+        color:'#EAEAEA', 
+        fontSize: 22
     },
 })
--- utils/encoding.tsx
+++ utils/encoding.tsx
@@ -5,7 +5,49 @@ import { DAYS, EventData } from "@/constants/EventTypes";
 import { EventColorsType } from "@/constants/EventColors";
 import { generateID } from "./eventTools";
 
-function HexColourToB94(hex:string):string {
+type RGBBytes = Uint8Array;   // length = 3
+type PackedTime = Uint8Array; // length = 3
+type TimeTuple = number[] | [number, number];
+
+type ThemeMapEntry = [string, EventColorsType];
+
+type MaximisedJSON = {
+    ThemeMap?: ThemeMapEntry[];
+    eventData?: EventData[];
+    [key: string]: unknown;
+};
+
+type MinifiedLessonBase = [
+    PackedTime,
+    string,
+    string,
+    string,
+    string
+];
+
+type MinifiedLessonWithGroup = [
+    PackedTime,
+    string,
+    string,
+    string,
+    string,
+    string
+];
+
+type MinifiedLesson = MinifiedLessonBase | MinifiedLessonWithGroup;
+
+type MinifiedColour = [
+    string,
+    RGBBytes,
+    RGBBytes
+];
+
+type MinifiedJSON= [
+    MinifiedLesson[],
+    MinifiedColour[]
+];
+
+function HexColourToB94(hex:string):RGBBytes {
     hex = hex.substring(1)
     const b = new Uint8Array([
         parseInt(hex.slice(0,2), 16),
@@ -13,18 +55,18 @@ function HexColourToB94(hex:string):string {
         parseInt(hex.slice(4,6), 16)
     ]);
 
-    return encodeB94(b);
+    return b //encodeB94(b);
 }
 
-function B94ToHexColour(cmp:string):string {
-    let bytes = decodeB94(cmp);
+function B94ToHexColour(bytes:RGBBytes):string {
+    // let bytes = decodeB94(cmp);
     
     return '#' + Array.from(bytes)
             .map(b => b.toString(16).padStart(2, '0'))
             .join('');
 }
 
-function bitPackTime(startTime: number[], endTime: number[], day:string){
+function bitPackTime(startTime: TimeTuple, endTime: TimeTuple, day:string):PackedTime {
     // xDDD_SSSS-SSSS_SSOO-OOOO_OOOO
     //  DAY ^--START---^^--OFFSET--^
 
@@ -40,17 +82,18 @@ function bitPackTime(startTime: number[], endTime: number[], day:string){
     packed_bytes[1] = (packed_day >> 8) & 0xff;
     packed_bytes[2] = packed_day & 0xff;
 
-    return encodeB94(packed_bytes)
+    return packed_bytes //encodeB94(packed_bytes)
 }
 
-function bitUnpackTime(encoded:string) {
+function bitUnpackTime(data:PackedTime) {
     // xDDD_SSSS-SSSS_SSOO-OOOO_OOOO
     //  DAY ^--START---^^--OFFSET--^
-    let data = decodeB94(encoded);
+    // const data = decodeB94(encoded);
+    const packed_day = (data[0] << 16) | (data[1] << 8) | data[2];
 
-    const dayInd = (data[0] >> 4) ^ 7
-    const start =  (data[0] ^ 0x0f << 6) + (data[1] >> 2) + 480
-    const offset = (data[1] ^ 3 << 8) + data[2]
+    const dayInd = (packed_day >> 20) & 0x07
+    const start =  ((packed_day >> 10) & 0x03ff) + 480
+    const offset = (packed_day      ) & 0x03ff
     const end = start + offset;
 
     const startTime = [ Math.floor(start/60), start % 60 ];
@@ -60,36 +103,36 @@ function bitUnpackTime(encoded:string) {
     return {startTime, endTime, day};
 }
 
-function minifyJSON(obj: Record<string, any>) {
-    let lessons:any[] = []
-    let colours:string[][] = []
+function minifyJSON(obj: MaximisedJSON):MinifiedJSON {
+    let lessons:MinifiedLesson[] = []
+    let colours:MinifiedColour[] = []
 
     for(let key of Object.keys(obj)){
         if(key == "ThemeMap"){
-            for(let [loc, col] of obj[key]) {
+            for(let [loc, col] of obj.ThemeMap!) {
                 colours.push([
                     loc, 
-                    HexColourToB94(col["background"]), 
-                    HexColourToB94(col["text"])
+                    HexColourToB94(col.background), 
+                    HexColourToB94(col.text)
                 ]);
             }
             continue;
         }
 
         if(key == "eventData"){
-            for (let lesson of obj[key]){
-                let enc_day = bitPackTime(lesson["startTime"], lesson["endTime"], lesson["day"])
+            for (let lesson of obj.eventData!){
+                let enc_day = bitPackTime(lesson.startTime, lesson.endTime, lesson.day)
 
-                let lsn = [
+                let lsn:MinifiedLessonBase = [
                     enc_day,
-                    lesson["location"],
-                    lesson["shortTitle"],
-                    lesson["teacher"],
-                    lesson["title"]
+                    lesson.location,
+                    lesson.shortTitle || "",
+                    lesson.teacher,
+                    lesson.title
                 ]; 
 
-                if(lesson["group"].length != 0)
-                    lsn.push(lesson["group"])
+                if(lesson.group.length != 0)
+                    lsn.push(lesson.group)
 
                 lessons.push(lsn);
             }
@@ -103,15 +146,17 @@ function minifyJSON(obj: Record<string, any>) {
     return [lessons, colours]
 }
 
-function maxifyJSON(obj: any[]) {
+function maxifyJSON(obj: MinifiedJSON):MaximisedJSON {
     const [lessons, colors] = obj
 
-    let res:Record<string, any> = {}
+    let res:MaximisedJSON = {}
 
     // theme rebuilding
     let themeMap = new Map<string, EventColorsType>()
     for(let [loc, bg, txt] of colors) {
-        themeMap.set(loc, {background: bg, text: txt, border: '#00000001' })
+        let background = B94ToHexColour(bg);
+        let text = B94ToHexColour(txt);
+        themeMap.set(loc, {background, text, border: '#00000001' })
     }
     res['ThemeMap'] = [...themeMap]
 
@@ -120,8 +165,8 @@ function maxifyJSON(obj: any[]) {
     for(let lsn of lessons) {
         // [enc_time, location, shortTitle, teacher, title, group?]
         let time = bitUnpackTime(lsn[0]);
-        let [location, shortTitle, teacher, title] = lsn.slice(1, 5)
-        let group = lsn.length > 5 ? lsn[5] : ""
+        let [location, shortTitle, teacher, title] = [lsn[1], lsn[2], lsn[3], lsn[4]]
+        let group = lsn.length > 5 ? lsn[5]! : ""
 
         let id = generateID(uncompressedLessons) || -1;
         uncompressedLessons.push({
@@ -137,6 +182,164 @@ function maxifyJSON(obj: any[]) {
     res['eventData'] = uncompressedLessons
 
     return res
+}
+
+const NULL_STR = 0;
+function serialize(data:MinifiedJSON){
+    const strMem = new Map<string, number>();
+    const strBank: number[] = [];
+    const encoder = new TextEncoder();
+    let strBankIndex = 1;
+
+    function packString(s: string): number {
+        if (strMem.has(s)) {
+        return strMem.get(s)!;
+        }
+
+        const encoded = encoder.encode(s);
+        if (encoded.length > 0xff) {
+        throw new Error(`'${s}' too long`);
+        }
+
+        const index = strBankIndex;
+        strBankIndex += 1;
+        strMem.set(s, index);
+
+        // append length and bytes to string bank
+        strBank.push(encoded.length);
+        for (let i = 0; i < encoded.length; i++) {
+        strBank.push(encoded[i]);
+        }
+
+        return index;
+    }
+    const [events, colours]  = data
+
+    const buff:number[] = []
+
+    buff.push(Math.min(events.length , 255))
+    buff.push(Math.min(colours.length, 255))
+
+    for(let [loc, bg, txt] of colours){
+        buff.push(packString(loc));
+        buff.push(bg[0], bg[1], bg[2]);
+        buff.push(txt[0], txt[1], txt[2]);
+    }
+
+    for(let lesson of events) {
+        const dt = lesson[0];
+        if (dt.length !== 3)
+            throw new Error("DAY_TIME must be length 3");
+
+        buff.push(dt[0], dt[1], dt[2]);
+        
+        for (let i = 1; i < 5; i++) 
+          buff.push( packString(lesson[i] as string) );
+
+        if(lesson.length > 5)
+            buff.push(packString(lesson[5] as string))
+        else 
+            buff.push(NULL_STR)
+    }
+
+    // append string bank
+    for (let i = 0; i < strBank.length; i++) 
+        buff.push(strBank[i]);
+
+    return [Uint8Array.from(buff), Uint8Array.from(strBank)];
+}
+
+function deserialize(buffer:Uint8Array):MinifiedJSON {
+    const decoder = new TextDecoder();
+    let offset = 0;
+
+    const lessonLen = buffer[offset++];
+    const colourLen = buffer[offset++];
+
+    // unpack colours
+    const coloursRaw:{loc:number, bg:RGBBytes, txt:RGBBytes}[] = []
+
+    for (let i = 0; i < colourLen; i++) {
+        const loc = buffer[offset++];
+
+        const bg = buffer.slice(offset, offset+3);
+        offset += 3;
+
+        const txt = buffer.slice(offset, offset+3);
+        offset += 3;
+
+        coloursRaw.push({ loc, bg, txt });
+    }
+
+    // unpack lessons
+    const lessonsRaw: {dt: PackedTime, strIndexes: number[]}[] = [];
+
+    for(let i = 0; i < lessonLen; i++) {
+        const dt = buffer.slice(offset, offset+3);
+        offset += 3;
+
+        const strIndexes = [
+            buffer[offset++], // location
+            buffer[offset++], // shortTitle
+            buffer[offset++], // teacher
+            buffer[offset++], // title
+            buffer[offset++]  // group
+        ];
+
+        lessonsRaw.push({dt, strIndexes})
+    }
+
+    // Reading string bank
+    const strBank = buffer.slice(offset);
+    const strTable: string[] = [];
+
+    let sbOffset = 0, strIndex = 0;
+
+    while( sbOffset < strBank.length) {
+        const len = strBank[sbOffset++];
+        const strBytes = strBank.slice(sbOffset, sbOffset+len);
+
+        sbOffset += len;
+
+        strTable[strIndex++] = decoder.decode(strBytes);
+    }
+
+    function unpackString(index: number): string {
+        if (index === NULL_STR) return "";
+        return strTable[index-1];
+    }
+
+    // rebuilding JSON
+    const lessons:MinifiedLesson[] = []
+    const colours:MinifiedColour[] = []
+
+    for(let {loc, bg, txt} of coloursRaw){
+        colours.push([
+            unpackString(loc),
+            bg,
+            txt
+        ]);
+    }
+
+    for(let {dt, strIndexes} of lessonsRaw) {
+        let unpacked = strIndexes.map(unpackString)
+        let base:MinifiedLesson = [
+           dt,
+            unpackString(strIndexes[0]),
+            unpackString(strIndexes[1]),
+            unpackString(strIndexes[2]),
+            unpackString(strIndexes[3]),
+            unpackString(strIndexes[4])
+        ];
+
+        if(strIndexes[5] != NULL_STR)
+            base.push(unpackString(strIndexes[5]))
+
+        lessons.push(base)
+    }
+
+    return [lessons, colours]
+
 }
 
 export const SHARE_LINK_BASE = 'ttshare://tt.app/data#'
@@ -163,25 +366,21 @@ export async function getCompressedData(){
 
     })
     .then((data) => {
-        return pako.deflate( JSON.stringify( minifyJSON(data) ) )
+        const [serial, dict] = serialize(minifyJSON(data))
+        // console.log(serial)
+        return pako.deflate( serial, {level: 9}) 
     })
 
 }
 
 
 export function decompressData(bytes:Uint8Array ){
-
-    for (let trim = 0; trim < 4; trim++) {
-        try {
-            const arr = trim === 0 ? bytes : bytes.slice(0, bytes.length - trim);
-            let unzipped = pako.inflate(arr);
-            let miniJson =  new TextDecoder().decode(unzipped)
-            return JSON.stringify(maxifyJSON(JSON.parse(miniJson)))
-        } catch (e) {
-            if (trim === 3) throw e;
-        }
-    }
-
+    let unzipped = pako.inflate(bytes); 
+    // let miniJson =  new TextDecoder().decode(unzipped)
+    // return JSON.stringify(maxifyJSON(JSON.parse(miniJson)))
+    const des = deserialize(unzipped)
+    // console.log(JSON.stringify( maxifyJSON( des ) ))
+    return JSON.stringify( maxifyJSON( des ) )
 }
 
 //// B94 ENCODING ////
